9a4bad0d5433c8039a993011d75fdd8c7dde6735
Mirko Argentino <mirko@bbn.solutions>
2022-10-19 09:04:46 +0200

    Working on webhook

--- src/mvc/public/webhooks/gitlab.php
+++ src/mvc/public/webhooks/gitlab.php
@@ -1,7 +1,6 @@
 <?php
 $haders = apache_request_headers();
 if (!empty($haders['X-Gitlab-Token'])) {
-  $idServer = $haders['X-Gitlab-Token'];
-  $ctrl->inc->vcs->changeServer($idServer)->analyzeWebhook($ctrl->post);
+  $ctrl->inc->vcs->changeServer($haders['X-Gitlab-Token'])->analyzeWebhook($ctrl->post);
 }
 $ctrl->obj->success = true;
